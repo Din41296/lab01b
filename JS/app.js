a0943ed352d1d56d42e46ef3911eb9b422512bac
Din41296 <dina.muasher@icloud.com>
2021-06-20 17:37:47 +0300

    Adding new files

---
+++ JS/app.js
@@ -0,0 +1,32 @@
+let userName = prompt('What is your name?');
+console.log(userName);
+alert("Hello");
+
+
+let userAge= prompt('How old are you?');
+console.log(userAge);
+alert("Welcome to my website.");
+
+
+
+let person = prompt('Do you like healthy food?');
+console.log(person)
+alert('I know you get bored,one question left');
+
+
+
+
+
+let favFood = prompt('What is your favorite food?');
+console.log(favFood);
+alert("ُEnjoy your meal");
+
+
+
+
+
+
+
+
+
+
